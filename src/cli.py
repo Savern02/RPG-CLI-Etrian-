a33592d80e1c598404fc60fdd9f8cd0a280ba24e
Savern02 <yoshishi.58@gmail.com>
2026-03-07 09:54:06 -0600

    added register and entry to the AI app

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,27 @@
+from typing import Annotated
 import typer
 
-def main(name: str):
-    print(f"Hello {name}")
+app = typer.Typer()
+
+@app.command() 
+def userReg(#ister
+    user: Annotated[str, typer.Argument()], #online name that the AI will call
+    age: Annotated[int, typer.Option(min=18)],#with ai needs to be over 18
+    ):
+        if(age < 18):
+            return 0 #add error
+        print(f"User is {user}")
+        print(f"--age is {age}")
+    
+
+@app.command()
+def rpg(mode: str):
+    if mode == "demo":
+        print("demo mode")
+        #make a rpg function that will set up 
+    else:
+        return 0 #add error for no mode set
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
